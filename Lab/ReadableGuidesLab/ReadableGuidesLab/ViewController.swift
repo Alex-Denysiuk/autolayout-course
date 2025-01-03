9776f89117198c07ad4f06e80510263eff956893
Alex Denusiuk <alexdenusyk@gmail.com>
2025-01-03 19:50:03 +0200

    lab: add readable content guide

---
+++ Lab/ReadableGuidesLab/ReadableGuidesLab/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ReadableGuidesLab
+//
+//  Created by Dev on 03.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews() {
+        let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+        let label = makeLabel(withText: text)
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
+            label.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor),
+            label.bottomAnchor.constraint(equalTo: view.readableContentGuide.bottomAnchor)
+        ])
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        label.font = .systemFont(ofSize: 20)
+        label.textColor = .black
+        label.numberOfLines = 0
+        return label
+    }
+}
